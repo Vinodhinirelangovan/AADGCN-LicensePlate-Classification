23517c90950daa2d91dd7e6ad96180c97079a0b9
Vinodhini R E <vinodhinirelangovan@gmail.com>
2025-06-02 12:48:30 +0530

    Delete code directory

--- code/image_read.m
+++
@@ -1,7 +0,0 @@
-function inputImage = image_read(imageFilePath)
-[~, name, ~] = fileparts(imageFilePath);
-label = name(1:2);
-load Model.mat
-save proposedmodel.mat net label
-inputImage=imread(imageFilePath);
-end
--- code/swinBlock.m
+++
@@ -1,7 +0,0 @@
-function layer = swinBlock(name)
-    layer = [
-        convolution2dLayer(3, 64, 'Padding','same','Name',[name '_conv'])
-        batchNormalizationLayer('Name',[name '_bn'])
-        reluLayer('Name',[name '_relu'])
-    ];
-end
